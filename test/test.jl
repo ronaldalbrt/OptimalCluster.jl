da5ec099e7bd337f14ff75e877821ca76361fc3b
Ronald Albert <ronaldalbert1609@gmail.com>
2023-09-12 05:12:17 -0300

    :sparkles: test

--- test/test.jl
+++ test/test.jl
@@ -21,18 +21,18 @@ data = OptimalCluster.HomogeneousCluster.ClusterProblemData(n, m, distances)
 
 
 model = OptimalCluster.HomogeneousCluster.jump_formulation(data)
-optimize!(model)
+opt_time = optimize!(model)
 
 output["obj_value"] = objective_value(model)
 
 n_steps = 100
-lower_bound = ceil(output["obj_value"])
+lower_bound = Int64(ceil(output["obj_value"]))
 ϵ = .5
 
 gap = 0.1
 
 initial_u = rand(data.n_points)
-best_solution, results = OptimalCluster.HomogeneousCluster.subgradient_algorithm(initial_u, data, .6, n_steps, lower_bound, ϵ; stop_gap=gap)
+best_solution, results =  OptimalCluster.HomogeneousCluster.subgradient_algorithm(initial_u, data, .6, n_steps, lower_bound, ϵ; stop_gap=gap)
 
 output["best_solution"] = best_solution
 output["results"] = results
